134685a832b812d47a7bccd215edc86d674469e8
farazkhan2373 <136571344+farazkhan2373@users.noreply.github.com>
2023-12-04 23:24:41 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ app.use(express.json());
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
 
+app.get('/api/get-next-num', (req, res) => {
+  // Check if the 'num' property exists in the request body
+  if (!req.body || !req.body.num || typeof req.body.num !== 'number') {
+    return res.status(400).json({ message: 'Invalid input. Please provide a valid number.', status: 'failure' });
+  }
+
+  // Get the number from the request body
+  const currentNumber = req.body.num;
+
+  // Calculate the next number
+  const nextNumber = currentNumber + 1;
+
+  // Respond with the next number and success status
+  res.json({ message: nextNumber, status: 'success' });
+});
+
 module.exports = app;
